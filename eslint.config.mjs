bf5f3afd9b6df12e76af021fb500e9c6a677f54d
Svitlana <missis.chuvaeva@ukr.net>
2025-08-01 22:59:35 +0300

    add config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
-]);
+export default [
+  pluginJs.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
+];

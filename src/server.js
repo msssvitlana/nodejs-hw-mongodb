89cc5467838c1785104368faab7b68ba68ca7720
Svitlana <missis.chuvaeva@ukr.net>
2025-08-01 23:52:34 +0300

    debugs

--- src/server.js
+++ src/server.js
@@ -54,14 +54,14 @@ export const setupServer = async () => {
     });
   });
 
-  //   app.use((req, res, next) => {
-  //     res.status(404).json({
-  //       message: 'Not found',
-  //     });
-  //   });
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
   app.use((err, req, res, next) => {
     res.status(500).json({
-      messge: 'Something went wrong',
+      message: 'Something went wrong',
       error: err.message,
     });
   });

9196d8c4f0092a450a1064695dc258d0ad96221e
Svitlana <missis.chuvaeva@ukr.net>
2025-08-02 15:34:55 +0300

    change index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 // src/index.js
 
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+async function start() {
+  try {
+    await initMongoConnection();
+    setupServer();
+  } catch (err) {
+    console.error('Failed to start application:', err);
+    process.exit(1);
+  }
+}
+
+start();

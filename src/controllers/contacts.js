1964427ddd8f2212fbc527ff1eb555a78ad761f7
Svitlana <missis.chuvaeva@ukr.net>
2025-08-08 22:44:21 +0300

    add last changes

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,10 +1,16 @@
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  createContact,
+  deleteContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res, next) => {
   try {
     const contacts = await getAllContacts();
-
+    // const body = req.body;
     res.json({
       status: 200,
       message: 'Successfully found contacts!',
@@ -33,5 +39,63 @@ export const getContactByIdController = async (req, res, next) => {
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
+};
+export const deleleteContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const upsertContactController = async (req, res, next) => {
+  const { contactId } = req.param;
+
+  const result = await updateContact(contactId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: 'Successfully upsert a contact!',
+    data: result.contact,
+  });
+};
+
+export const patchContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
   });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,11 +1,23 @@
 import { Router } from 'express';
 import {
+  createContactController,
+  deleleteContactController,
   getContactByIdController,
   getContactsController,
+  patchContactController,
+  upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 router.get('/contacts', getContactsController);
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleleteContactController));
+
+router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 export default router;

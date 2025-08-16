c34ebeadfaf7a7c5a3dde67318455baf731c63f7
Svitlana <missis.chuvaeva@ukr.net>
2025-08-16 19:04:41 +0300

    debug filter

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -14,7 +14,7 @@ export const getContactsController = async (req, res, next) => {
   try {
     const { page, perPage } = parsePaginationParams(req.query);
     const { sortBy, sortOrder } = parseSortParams(req.query);
-    const filter = parseFilterParams(req.params);
+    const filter = parseFilterParams(req.query);
 
     const contacts = await getAllContacts({
       page,

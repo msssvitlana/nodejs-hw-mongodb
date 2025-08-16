72e8e539bb5869b222143d621d3470fb11e371ea
Svitlana <missis.chuvaeva@ukr.net>
2025-08-16 19:17:48 +0300

    debug validateBody

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,13 +3,13 @@ import createHttpError from 'http-errors';
 export const validateBody = (schema) => async (req, res, next) => {
   try {
     await schema.validateAsync(req.body, {
-      abortEaly: false,
+      abortEarly: false,
     });
     next();
   } catch (err) {
     const error = createHttpError(400, 'Bad Request', {
       errors: err.details,
     });
+    next(error);
   }
-  next(error);
 };

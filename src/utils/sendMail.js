47380245322aced7fcbbd1c20bab638d2e373ac8
SvitLana <missis.chuvaeva@ukr.net>
2025-09-01 23:46:29 +0300

    Hw6 email and images (#41)
    * reset password
    
    * reset password
    
    * upload file
    
    * env.example
    
    * debug routers
    
    * debug

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,16 +1,27 @@
-import nodemailer from 'nodemailer';
-import { SMTP } from '../constants.js';
-import { getEnvVar } from './getEnvVar.js';
+// src/utils/sendMail.js
 
-const transporter = nodemailer.createTransport({
-  host: getEnvVar(SMTP.SMTP_HOST),
-  port: Number(getEnvVar(SMTP.SMTP_PORT)),
-  auth: {
-    user: getEnvVar(SMTP.SMTP_USER),
-    pass: getEnvVar(SMTP.SMTP_PASSWORD),
-  },
-});
+import nodemailer from 'nodemailer';
+
+import { SMTP } from '../constants/index.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+let transporter;
+
+const initTransporter = () => {
+  if (!transporter) {
+    transporter = nodemailer.createTransport({
+      host: getEnvVar(SMTP.SMTP_HOST),
+      port: Number(getEnvVar(SMTP.SMTP_PORT)),
+      auth: {
+        user: getEnvVar(SMTP.SMTP_USER),
+        pass: getEnvVar(SMTP.SMTP_PASSWORD),
+      },
+    });
+  }
+  return transporter;
+};
 
 export const sendEmail = async (options) => {
-  return await transporter.sendEmail(options);
+  const mailer = initTransporter();
+  return await mailer.sendMail(options);
 };

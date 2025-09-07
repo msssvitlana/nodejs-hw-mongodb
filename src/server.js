547f48cba4a6d095709faf2f4cfe45da91c40af2
SvitLana <missis.chuvaeva@ukr.net>
2025-09-07 22:15:49 +0300

    Hw7 swagger (#46)
    * add googleOAuth
    
    * swagger
    
    * debug

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 dotenv.config();
 const PORT = Number(getEnvVar('PORT', '3000'));
 
